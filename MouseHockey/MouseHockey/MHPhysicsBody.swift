b373c09cfe385481191b4db33f8f4f54d5e97da1
KayhanQ <kayhanfqaiser@gmail.com>
2016-02-24 13:54:32 -0500

    add extensions from SKUtils

---
+++ MouseHockey/MouseHockey/MHPhysicsBody.swift
@@ -0,0 +1,9 @@
+//
+//  MHPhysicsBody.swift
+//  MouseHockey
+//
+//  Created by Kayhan Qaiser on 2016-02-24.
+//  Copyright © 2016 Kayhan Qaiser. All rights reserved.
+//
+
+import Foundation
---
+++ MouseHockey/MouseHockey/SKPhysicsBody+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  SKPhysicsBody+Extensions.swift
+//  MouseHockey
+//
+//  Created by Kayhan Qaiser on 2016-02-24.
+//  Copyright © 2016 Kayhan Qaiser. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+public extension SKPhysicsBody {
+
+    
+
+}
